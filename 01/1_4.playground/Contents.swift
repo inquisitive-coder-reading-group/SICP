cf49a4b550bd4ad05d5ce9ad382ba3ba61c160c3
mattthousand <matt.buckley@raizlabs.com>
2016-03-05 14:47:01 -0500

    First pass at exercise 1.4.

---
+++ 01/1_4.playground/Contents.swift
@@ -0,0 +1,13 @@
+/*
+Exercise 1.4: Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+((if (> b 0) + -) a b))
+
+*/
+
+/**
+*  An `if` conditional returns an addition operator if b
+*  is greater than zero, otherwise it returns a subtraction 
+*  operator. Then, that operator is applied to operands a and b.
+*/
