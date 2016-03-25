9b82ea0c398192c36f0d10526b4111dcc0ae42bb
mattthousand <matt.buckley@raizlabs.com>
2016-03-25 12:16:11 -0400

    Make 1.3 a little more generic.

--- 01/1_3.playground/Contents.swift
+++ 01/1_3.playground/Contents.swift
@@ -4,7 +4,8 @@ import Foundation
 Exercise 1.3: Define a procedure that takes 3 numbers as arguments and returns the sum of the squares of the 2 larger numbers
 */
 
-func sumOfSquares(a: Int, b: Int, c: Int) -> Int {
-    let squares = [a, b, c].sort({ $0 < $1 }).dropFirst(1).map({ $0 * $0 })
-    return squares.reduce(0, combine: +)
+func sumOfSquares<T: IntegerArithmeticType>(a: T, b: T, c: T) -> T {
+    let squares = [a, b, c]
+    let sortedSquares = squares.sort({ $0 < $1 }).dropFirst(1).map({ $0 * $0 })
+    return sortedSquares.reduce(0 as! T, combine: +)
 }
