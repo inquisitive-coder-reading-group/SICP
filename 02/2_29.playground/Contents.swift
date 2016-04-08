8782f81232cccb492ef071bc8e697830dd311912
Alex Lew <alexl.mail@gmail.com>
2016-04-08 01:16:50 -0400

    Add single enum-based example for comparison

--- 02/2_29.playground/Contents.swift
+++ 02/2_29.playground/Contents.swift
@@ -1,4 +1,4 @@
-/* Exercise 2.29:
+/* Exercise 2.9:
     A binary mobile consists of two branches, a left branch and a right branch. Each branch
     is a rod of a certain length, from which hangs either a weight or another binary
     mobile. We can represent a binary mobile using compound data by constructing it from
@@ -74,6 +74,24 @@ let balancedMobile1 = Mobile(left: Mobile.Branch(length: 5, structure: .Weight(8
 let balancedMobile2 = Mobile(left: Mobile.Branch(length: 1, structure: .SubMobile(balancedMobile1)),
                              right: Mobile.Branch(length: 4, structure: .Weight(3)))
 
+
+/* It is perhaps also worth considering another version of this data structure,
+   which collapses the three types into one: an enum. Unlike Mobile, MobileV2
+   is allowed to be a single weight, unattached to a branch. */
+
+enum MobileV2 {
+    case Weight(Int)
+    indirect case Node(left: MobileV2, leftLength: Int, right: MobileV2, rightLength: Int)
+}
+
+let balancedMobile1V2 = MobileV2.Node(left: .Weight(8), leftLength: 5, right: .Weight(4), rightLength: 10)
+
+let balancedMobile2V2 = MobileV2.Node(left: balancedMobile1V2, leftLength: 1, right: .Weight(3), rightLength: 4)
+
+/* Below, we continue to develop MobileV2, so that we can see how it compares to the
+   more literal translation of the book. */
+
+
 /* (a) Write the corresponding selectors left-branch and right-branch, which return the branches of a mobile, and branch-length and branch-structure, which re- turn the components of a branch. */
 
 /* Commentary: These are kind of silly to write in Swift, because we can just use 
@@ -92,8 +110,59 @@ extension Mobile.Branch {
     func branchStructure() -> Mobile.Structure { return structure }
 }
 
-
 balancedMobile1.leftBranch().branchLength()
+
+/* It is unnecessary to translate this for MobileV2, which does not have
+   the concept of a branch or structure. But we can write some basic
+   accessors. They require quite a bit of boilerplate... point 1 against MobileV2. */
+
+extension MobileV2 {
+    func leftBranchLength() -> Int? {
+        switch self {
+        case .Node(left: _, leftLength: let l, right: _, rightLength: _):
+            return l
+        case .Weight(_):
+            return nil
+        }
+    }
+    
+    func rightBranchLength() -> Int? {
+        switch self {
+        case .Node(left: _, leftLength: _, right: _, rightLength: let l):
+            return l
+        case .Weight(_):
+            return nil
+        }
+    }
+    
+    func leftSubMobile() -> MobileV2? {
+        switch self {
+        case .Node(left: let m, leftLength: _, right: _, rightLength: _):
+            return m
+        case .Weight(_):
+            return nil
+        }
+    }
+    
+    func rightSubMobile() -> MobileV2? {
+        switch self {
+        case .Node(left: _, leftLength: _, right: let m, rightLength: _):
+            return m
+        case .Weight(_):
+            return nil
+        }
+    }
+    
+    func isWeight() -> Bool {
+        if case .Weight(_) = self {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+balancedMobile1V2.leftBranchLength()
 
 
 /* (b) Using your selectors, define a procedure total-weight that returns the total 
@@ -123,6 +192,25 @@ extension Mobile {
 
 balancedMobile1.totalWeight() // 8 + 4 = 12
 balancedMobile2.totalWeight() // 12 + 3 = 15
+
+
+/* Because the data structure is simpler, it's easier to write a simple recursive
+   function for MobileV2: */
+
+extension MobileV2 {
+    func totalWeight() -> Int {
+        switch self {
+        case .Weight(let w):
+            return w
+        case .Node(left: let l, leftLength: _, right: let r, rightLength: _):
+            return l.totalWeight() + r.totalWeight()
+        }
+    }
+}
+
+balancedMobile1V2.totalWeight()
+balancedMobile2V2.totalWeight()
+
 
 /* (c) A mobile is said to be balanced if the torque applied by its top-left branch is equal to that applied by its top-right branch (that is, if the length of the left rod multiplied by the weight hanging from that rod is equal to the corresponding product for the right side) and if each of the submobiles hanging off its branches is balanced. Design a predicate that tests whether a binary mobile is balanced. */
 
@@ -162,6 +250,37 @@ balancedMobile1.isBalanced()
 balancedMobile2.leftTorque() // 12 * 1
 balancedMobile2.rightTorque() // 4 * 3
 balancedMobile2.isBalanced()
+
+
+/* As with totalWeight, the simple recursive enum makes this function easy, too. */
+
+extension MobileV2 {
+    func leftTorque() -> Int? {
+        guard let leftLen = leftBranchLength(),
+            left = leftSubMobile() else { return nil }
+        return left.totalWeight() * leftLen
+    }
+    
+    func rightTorque() -> Int? {
+        guard let rightLen = rightBranchLength(),
+            right = rightSubMobile() else { return nil }
+        return right.totalWeight() * rightLen
+    }
+    
+    func isBalanced() -> Bool {
+        // If we don't have a left/right submobile, we are balanced by default
+        guard let left = leftSubMobile(), right = rightSubMobile() else { return true }
+        return leftTorque() == rightTorque() && left.isBalanced() && right.isBalanced()
+    }
+}
+
+balancedMobile1V2.leftTorque() // 5 * 8
+balancedMobile1V2.rightTorque() // 10 * 4
+balancedMobile1V2.isBalanced()
+
+balancedMobile2V2.leftTorque() // 12 * 1
+balancedMobile2V2.rightTorque() // 4 * 3
+balancedMobile2V2.isBalanced()
 
 
 /* (d) Suppose we change the representation of mobiles so that the constructors are
