f7a965b8d320ac58232ae4122e64e2389a6ee4b7
mattthousand <matt.buckley@raizlabs.com>
2016-04-13 09:53:53 -0400

    Create SICPUtils framework.

---
+++ SICPUtils/SICPUtils/SICPUtils.h
@@ -0,0 +1,19 @@
+//
+//  SICPUtils.h
+//  SICPUtils
+//
+//  Created by Matthew Buckley on 4/13/16.
+//  Copyright © 2016 Swift Book Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SICPUtils.
+FOUNDATION_EXPORT double SICPUtilsVersionNumber;
+
+//! Project version string for SICPUtils.
+FOUNDATION_EXPORT const unsigned char SICPUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SICPUtils/PublicHeader.h>
+
+
